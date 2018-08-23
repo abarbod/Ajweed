f1ee8f14fc90708e7d3c4eb6bcc645f27b38d975
Hamoud Alhoqbani <h.alhoqbani@gmail.com>
2018-08-22 22:29:34 -0700

    Add show profile page and "My Account" nav item

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -53,7 +53,14 @@
                                     {{ Auth::user()->name }} <span class="caret"></span>
                                 </a>
 
+
                                 <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+                                    <a class="dropdown-item" href="{{ route('users.profile.show', auth()->user()) }}">
+                                        {{ __('My Account') }}
+                                    </a>
+
+                                    <div class="dropdown-divider"></div>
+
                                     <a class="dropdown-item" href="{{ route('logout') }}"
                                        onclick="event.preventDefault();
                                                      document.getElementById('logout-form').submit();">
--- resources/views/users/profiles/show.blade.php
+++ resources/views/users/profiles/show.blade.php
@@ -1,1 +1,36 @@
 @extends('layouts.app')
+<?php
+/**
+ * Profile page. To show the user's profile.
+ *
+ * @var \App\Models\User    $user The user associated with the profile.
+ * @var \App\Models\Profile $profile The users's profile.
+ */
+
+?>
+@section('content')
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-8">
+                <div class="card">
+                    <div class="card-header">{{ $user->name }}</div>
+
+                    <div class="card-body">
+                        @if (session('status'))
+                            <div class="alert alert-success" role="alert">
+                                {{ session('status') }}
+                            </div>
+                        @endif
+
+                        <p><strong>Gender: </strong>{{ $profile->gender }}</p>
+                        <p><strong>Birth Date: </strong>{{ $profile->birth_date->toDateString() }}</p>
+                    </div>
+
+                    <div class="card-footer">
+                        <a class="btn btn-info float-right" href="{{ route('users.profile.edit', $user) }}">@lang('Edit')</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
