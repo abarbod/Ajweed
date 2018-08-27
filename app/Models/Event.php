6010345434608e3f27843003650c2bf52fa78eab
Hamoud Alhoqbani <h.alhoqbani@gmail.com>
2018-08-27 16:30:29 -0700

    Generate Event model

---
+++ app/Models/Event.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+/**
+ * @property int $id
+ * @property \Carbon\Carbon $created_at
+ * @property \Carbon\Carbon $updated_at
+ */
+class Event extends Model
+{
+    //
+}
