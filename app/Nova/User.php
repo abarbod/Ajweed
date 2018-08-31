e0f3d075232b99cba29138e4e77bee148244b01c
Hamoud Alhoqbani <h.alhoqbani@gmail.com>
2018-08-30 22:52:20 -0700

    Add basic user details to admin dashboard

--- app/Nova/User.php
+++ app/Nova/User.php
@@ -30,13 +30,16 @@ class User extends Resource
      * @var array
      */
     public static $search = [
-        'id', 'name', 'email',
+        'id',
+        'name',
+        'email',
     ];
 
     /**
      * Get the fields displayed by the resource.
      *
-     * @param  \Illuminate\Http\Request  $request
+     * @param  \Illuminate\Http\Request $request
+     *
      * @return array
      */
     public function fields(Request $request)
@@ -44,29 +47,41 @@ class User extends Resource
         return [
             ID::make()->sortable(),
 
-            Gravatar::make(),
+            Gravatar::make(__('Avatar')),
 
-            Text::make('Name')
+            Text::make(__('Name'), 'name')
                 ->sortable()
                 ->rules('required', 'max:255'),
 
-            Text::make('Email')
+            Text::make(__('E-Mail Address'), 'email')
                 ->sortable()
                 ->rules('required', 'email', 'max:255')
                 ->creationRules('unique:users,email')
                 ->updateRules('unique:users,email,{{resourceId}}'),
 
-            Password::make('Password')
-                ->onlyOnForms()
-                ->creationRules('required', 'string', 'min:6')
-                ->updateRules('nullable', 'string', 'min:6'),
+            Password::make(__('Password'), 'password')
+                    ->onlyOnForms()
+                    ->creationRules('required', 'string', 'min:6')
+                    ->updateRules('nullable', 'string', 'min:6'),
+
+            Text::make(__('Saudi Id / Iqama Id'), 'official_id')
+                ->hideFromIndex()
+                ->rules(['required', 'digits:10',])
+                ->creationRules(['unique:users'])
+                ->updateRules(['unique:users,official_id,{{resourceId}}']),
+
+            Text::make(__('Mobile Number'), 'mobile')
+                ->rules(['required', 'regex:/^05\d{8}$/'])
+                ->creationRules('unique:users')
+                ->updateRules(['unique:users,mobile,{{resourceId}}']),
         ];
     }
 
     /**
      * Get the cards available for the request.
      *
-     * @param  \Illuminate\Http\Request  $request
+     * @param  \Illuminate\Http\Request $request
+     *
      * @return array
      */
     public function cards(Request $request)
@@ -77,7 +92,8 @@ class User extends Resource
     /**
      * Get the filters available for the resource.
      *
-     * @param  \Illuminate\Http\Request  $request
+     * @param  \Illuminate\Http\Request $request
+     *
      * @return array
      */
     public function filters(Request $request)
@@ -88,7 +104,8 @@ class User extends Resource
     /**
      * Get the lenses available for the resource.
      *
-     * @param  \Illuminate\Http\Request  $request
+     * @param  \Illuminate\Http\Request $request
+     *
      * @return array
      */
     public function lenses(Request $request)
@@ -99,11 +116,33 @@ class User extends Resource
     /**
      * Get the actions available for the resource.
      *
-     * @param  \Illuminate\Http\Request  $request
+     * @param  \Illuminate\Http\Request $request
+     *
      * @return array
      */
     public function actions(Request $request)
     {
         return [];
     }
+
+    /**
+     * Get the displayble label of the resource.
+     *
+     * @return string
+     */
+    public static function label()
+    {
+        return __('Users');
+    }
+
+    /**
+     * Get the displayble singular label of the resource.
+     *
+     * @return string
+     */
+    public static function singularLabel()
+    {
+        return __('User');
+    }
+
 }
