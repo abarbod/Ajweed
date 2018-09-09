76c6b300aaef137a03a5cfb90a3ff9fd02491530
Hamoud Alhoqbani <h.alhoqbani@gmail.com>
2018-09-09 07:58:44 -0700

    Add latest events to front page

--- resources/js/app.js
+++ resources/js/app.js
@@ -128,4 +128,47 @@ $(document).ready(function () {
         ]
 
     });
+    $('.events-slick').slick({
+        rtl: true,
+        dots: true,
+        arrows: false,
+        infinite: true,
+        speed: 300,
+        slidesToShow: 4,
+        slidesToScroll: 4,
+        responsive: [
+            {
+                breakpoint: 992,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 2,
+                    arrows: true,
+                    infinite: false,
+                    dots: true
+                }
+            },
+            {
+                breakpoint: 768,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 2,
+                    arrows: true,
+                    dots: true
+                }
+            },
+            {
+                breakpoint: 576,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1,
+                    arrows: true,
+                    dots: true,
+                }
+            },
+            // You can unslick at a given breakpoint now by adding:
+            // settings: "unslick"
+            // instead of a settings object
+        ]
+
+    });
 });
